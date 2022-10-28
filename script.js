241334ce10c88c8aee7181c6e571b0e768657b8b
Joseph David <jodavid444dave@gmail.com>
2022-10-28 03:18:20 -0700

    Dynamically create speakers section and implemented all requirements as instructed

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 const speakers = {
     one: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Crosby.0368d0d0.png',
+        title: 'Niall Crosby',
+        decription: 'CEO / Founder',
+        descriptioninfo: 'Providing a library that supports multiple frameworks is a huge challenge. Having spent years working on this problem, my team an I came up with a way for our grid component to support all frameworks,',
     },
     two: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Maran.e0be23bd.png',
+        title: 'Nivetha Maran',
+        decription: 'Software Engineer Pro',
+        descriptioninfo: 'In this talk, we will learn all about the react hooks. Starting from what are the main advantages of react hooks, different type of hooks, and how we can replace redux with react hooks in most of the applications with live examples.',
     },
     three: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Hawkins.d3e858f0.png',
+        title: 'Tyler Hawkins',
+        decription: 'Senior Software Engineer',
+        descriptioninfo: 'Test-driven development, or TDD, is a programming paradigm in which you write your tests first and your source code second. TDD is perfect when you’re writing code that has clear inputs and outputs, like pure functions',
     },
 
     four: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Derks.99bf843a.png',
+        title: 'Roy Derks',
+        decription: 'Developer Experience',
+        descriptioninfo: 'Type systems like TypeScript are important when developing JavaScript applications. They make sure that your props are passed in the correct form and shape throughout your application. Together with GraphQLs query ,',
     },
 
     five: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Rakowski.317b77c2.png',
+        title: 'Filip Rakowski',
+        decription: 'Co-founder @ CTO',
+        descriptioninfo: ' Co-founder & CTO of Vue Storefront, biggest Open Source eCommerce frontend project, co-organizer of Wrocław Vue.js.Learn PRPL Pattern and how to use it with React 4,  and dealing with common performance bottlenecks.',
     },
-
     six: {
-        image: 'images/speaker_05.png',
-        title: 'Joseph David',
-        decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
-        descriptioninfo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos? dipisicing elit. Quos?',
+        image: 'images/Kehl.6d83b400.png',
+        title: 'Griffin Solot-Kehl',
+        decription: 'Developer Advocate',
+        descriptioninfo: 'React has made development intuitive for developers of all skill levels with UI kits. This way, different products can develop a plug and play solution that abstracts away the complicated parts of their SDKs, to improve developers.',
     }
 
 }
@@ -56,6 +55,7 @@ main.innerHTML = `
 <hr class="feature_hr">
 </div>
 <div class="feature_main_container">
+<div class="feature_sub">
 <div class="feature_container ">
     <div class="feature_item">
         <img src="${speakers.one.image}" alt="">
@@ -82,6 +82,8 @@ main.innerHTML = `
     <h4>${speakers.two.descriptioninfo}</h4>
 </div>
 </div>
+</div>
+<div class="feature_sub">
 <div class="feature_container ">
     <div class="feature_item">
         <img src="${speakers.three.image}" alt="">
@@ -97,42 +99,47 @@ main.innerHTML = `
 </div>
 <div class="feature_container ">
     <div class="feature_item">
-        <img src="${speakers.two.image}" alt="">
+        <img src="${speakers.five.image}" alt="">
     </div>
     <div class="feature_sub_item">
-    <h2>${speakers.two.title}</h2>
-    <h3>${speakers.two.decription}</h3>
+    <h2>${speakers.five.title}</h2>
+    <h3>${speakers.five.decription}</h3>
     <div class="feature_sub_hr">
         <hr class="feature-hr">
     </div>
-    <h4>${speakers.two.descriptioninfo}</h4>
+    <h4>${speakers.five.descriptioninfo}</h4>
+</div>
+</div>
+</div>
+<div class="feature_sub">
+<div class="feature_container">
+    <div class="feature_item">
+        <img src="${speakers.four.image}" alt="">
+    </div>
+    <div class="feature_sub_item">
+    <h2>${speakers.four.title}</h2>
+    <h3>${speakers.four.decription}</h3>
+    <div class="feature_sub_hr">
+        <hr class="feature-hr">
+    </div>
+    <h4>${speakers.four.descriptioninfo}</h4>
 </div>
 </div>
 <div class="feature_container ">
     <div class="feature_item">
-        <img src="${speakers.two.image}" alt="">
+        <img src="${speakers.six.image}" alt="">
     </div>
     <div class="feature_sub_item">
-    <h2>${speakers.two.title}</h2>
-    <h3>${speakers.two.decription}</h3>
+    <h2>${speakers.six.title}</h2>
+    <h3>${speakers.six.decription}</h3>
     <div class="feature_sub_hr">
         <hr class="feature-hr">
     </div>
-    <h4>${speakers.two.descriptioninfo}</h4>
+    <h4>${speakers.six.descriptioninfo}</h4>
 </div>
 </div>
-<div class="feature_container ">
-    <div class="feature_item">
-        <img src="${speakers.two.image}" alt="">
-    </div>
-    <div class="feature_sub_item">
-    <h2>${speakers.two.title}</h2>
-    <h3>${speakers.two.decription}</h3>
-    <div class="feature_sub_hr">
-        <hr class="feature-hr">
-    </div>
-    <h4>${speakers.two.descriptioninfo}</h4>
-</div>
+ </div>
+
 </div>
 
 </div>
